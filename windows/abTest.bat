cd7fe814252bf048d71e49fd16078199595c764b
lishaoping <1913796030@qq.com>
2018-03-31 17:16:45 +0800

    update

---
+++ windows/abTest.bat
@@ -0,0 +1,4 @@
+D:\
+cd D:\download\httpd-2.4.32-o102n-x64-vc14\Apache24\bin
+ab -n 300 20 http://10.8.8.147:8001/app/ershoufang/home.shtml?lon=116.4081980&lat=39.9046670&topcate=house&city=bj&os=android&brand=HUAWEI&cversion=8.1.5&n_city=1&currentcid=1
+pause
