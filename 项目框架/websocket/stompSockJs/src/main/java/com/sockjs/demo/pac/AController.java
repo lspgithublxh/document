6320c0f0e89366f4451c53d64edc5539d38645f0
471126124lxh@sina.com <471126124lxh@sina.com>
2020-12-14 19:28:53 +0800

    tomcat-embed，websocket

---
+++ 项目框架/websocket/stompSockJs/src/main/java/com/sockjs/demo/pac/AController.java
@@ -0,0 +1,29 @@
+package com.sockjs.demo.pac;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * js订阅不是显式的。并不需要后端显式明确每个主题。
+ * 生产者则是显式的，调用下面的http接口就可以了。后端就发送给了其他方
+ * @Classname AController
+ * @Description TODO
+ * @Date 2020/12/14 15:15
+ * @Created by lisp-b
+ */
+@Controller
+public class AController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @GetMapping("refresh/{groupId}")
+    public void refresh(@PathVariable Long groupId) {///topic/echoTest/price
+        //将会向 订阅了/user/{groupId}/refresh的客户端发送"refresh"字符串。
+        simpMessagingTemplate.convertAndSendToUser("echoTest", "/price", "refresh");
+    }
+}
---
+++ 项目框架/websocket/stompWs/src/main/java/com/wls/demo/pac/WebSocketConfig.java
@@ -0,0 +1,61 @@
+package com.wls.demo.pac;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Date;
+
+/**
+ * @Classname AC
+ * @Description TODO
+ * @Date 2020/12/14 9:43
+ * @Created by lisp-b
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer  {
+
+//    @Autowired
+//    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //添加一个/websocket端点，客户端就可以通过这个端点来进行连接；withSockJS作用是添加SockJS支持
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("*")
+//                .withSockJS()
+        ;//.withSockJS()
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //定义了两个客户端订阅地址的前缀信息，也就是客户端接收服务端发送消息的前缀信息
+        registry.enableSimpleBroker("/topic","/queue");
+        //定义了服务端接收地址的前缀，也即客户端给服务端发消息的地址前缀
+        registry.setUserDestinationPrefix("/topic");
+//        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+//    @Scheduled(cron = "*/10 * * * * ?")//运行周期时间可配
+//    public void publicMarketValueMessage() {
+//        //这里的nowDate 暂且作为订阅的内容--可更换为具体的业务数据
+//        String nowDate = "ttt";
+//        //这里的destination 是 订阅的地址
+//        String destination = "/topic/echoTest/price";
+//        log.info("websocket 【{}】定时任务发布消息==>【开始】", destination);
+//        try {
+//            this.simpMessagingTemplate.convertAndSend(destination, nowDate);
+//        } catch (Exception e) {
+//            log.error("websocket ={} 定时任务发布消息==>【异常】", destination, e);
+//        }
+//    }
+}
